f4073c2002c04cb4286bd3b64ca105da51c04304
naitalamara <ouali.naitalamara@gmail.com>
2020-03-11 10:56:43 +0100

    numero de teleph

--- exo4_1/src/main/java/uvsq21807569/exo4_1/Personnel.java
+++ exo4_1/src/main/java/uvsq21807569/exo4_1/Personnel.java
@@ -1,7 +1,15 @@
 package uvsq21807569.exo4_1;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Personnel implements Personneltype {
 
+	private String nom ;
+	private String prenom ;
+	private List<NumeroTelephone>numerotelephone ;
+	private LocalDate datenaissance ;
+	private String fonction ;
 	
 	
 	public void affichage() {
